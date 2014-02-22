1a37a3acc16b197e8ed1427c2f0b670ad10ca6fa
Hannes Haataja <hanneksen@gmail.com>
2014-02-22 22:24:34 +0200

    Implemented deleting for lists & list items.

--- js/tilss.js
+++ js/tilss.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
   // Knouckout Models and Views //
+  var DB_REF = "https://tilss.firebaseio.com/lists";
 
   function ListItemModel(itemData){
     var self = this;
     self.itemid = ko.observable(itemData ? itemData.name() : '');
+    self.listid = itemData ? itemData.val().listid : '';
     self.item = ko.observable(itemData ? itemData.val().item : '');
+
+    self.deleteItem = function(item) {
+      var listid = item.listid;
+      var itemid = item.itemid();
+      var myDataRef = new Firebase(DB_REF);
+      myDataRef.child(listid).child("items").child(itemid).remove();
+    };
   }
 
   function ListModel(snapshot) {
@@ -14,19 +23,39 @@ $(document).ready(function(){
     self.listname = ko.observable(snapshot ? snapshot.val().listname : '');
 
     self.items = ko.observableArray();
-    var myDataRef = new Firebase('https://tilss.firebaseio.com/lists');
+
+    // Hook Firebase callbacks
+
+    // List item added
+    var myDataRef = new Firebase(DB_REF);
     myDataRef.child(self.listid()).child("items").on('child_added', function(snapshot) {
       self.items.push(new ListItemModel(snapshot));
+    });
+
+    // List item deleted
+    myDataRef.child(self.listid()).child("items").on('child_removed', function(snapshot) {
+      var itemid = snapshot.name();
+      var len = self.items().length;
+      while(len--){
+        if(self.items()[len].itemid() == itemid){
+           self.items.splice(len,1);
+        }
+      }
     });
 
     self.addNewItem = function(list){
       var listid = list.listid();
       var newitem = $('#newitem'+list.listid()).val();
-      //self.items.push(newitem);
-      var myDataRef = new Firebase('https://tilss.firebaseio.com/lists');
+      var myDataRef = new Firebase(DB_REF);
       myDataRef.child(listid).child("items").push({listid: listid, item: newitem});
       $('#newitem'+list.listid()).val('');
-    }
+    };
+
+    self.deleteList = function(list){
+      var listid = list.listid();
+      var myDataRef = new Firebase(DB_REF);
+      myDataRef.child(listid).remove();
+    };
   }
 
   function AppViewModel(){
@@ -34,10 +63,23 @@ $(document).ready(function(){
     self.lists = ko.observableArray();
     self.username = ko.observable("Jorkki");
 
-    // Hook firebase db for updates
-    var myDataRef = new Firebase('https://tilss.firebaseio.com/lists');
+    // Hook Firebase callbacks
+
+    // New list added
+    var myDataRef = new Firebase(DB_REF);
     myDataRef.on('child_added', function(snapshot) {
       self.lists.push(new ListModel(snapshot));
+    });
+
+    // Lists deleted
+    myDataRef.on('child_removed', function(snapshot) {
+      var listid = snapshot.name();
+      var len = self.lists().length;
+      while(len--){
+        if(self.lists()[len].listid() == listid){
+           self.lists.splice(len,1);
+        }
+      }
     });
 
     self.addNewList = function(listData){
@@ -46,6 +88,7 @@ $(document).ready(function(){
       myDataRef.push({listname: listname, username: username});
       $('#listname').val('');
     };
+
   }
 
   // Activates knockout.js
